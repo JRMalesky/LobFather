40e816f7beec4c1043f348dbdf103aa92dc684f6
Eric Peterford <epeterford@knights.ucf.edu>
2016-12-13 17:03:42 -0500

    Splash screen and menu music working

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        StartCoroutine("TransitionToMenu");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+    IEnumerator TransitionToMenu()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(1);
+    }
+}
